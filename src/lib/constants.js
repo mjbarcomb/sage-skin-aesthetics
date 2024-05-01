cc61ae94098baf4dcca16285b800f5def99920b4
Matt Barcomb <mbarcomb@me.com>
2024-04-30 21:32:28 -0400

    Name update

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,7 +3,7 @@ export const HERO = {
 		main: '/brie.webp',
 		bg: '/mesh-930.webp'
 	},
-	text: `Sage Skin Aesthetics, owned by solo paramedical aesthetician Briana, is an clinic offering treatments for all skin issues, specializing in all types of acne. Briana — a certified Acne Expert and finalist for the 2023 Acne Expert of the Year — offers a variety of treatments including Hydrafacial, DMK Enzyme Therapy, and many more.`
+	text: `Sage Skin Aesthetics, owned by solo paramedical aesthetician Brianna, is an clinic offering treatments for all skin issues, specializing in all types of acne. Brianna — a certified Acne Expert and finalist for the 2023 Acne Expert of the Year — offers a variety of treatments including Hydrafacial, DMK Enzyme Therapy, and many more.`
 };
 
 export const BOOKING_LINK = 'https://www.vagaro.com/sageskinaesthetics/book-now';
